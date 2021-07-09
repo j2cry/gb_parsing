b33bab9f9b8517c263861c8aa959f19a3ad9c967
avagadro <j2cry.avagadro@gmail.com>
2021-07-09 23:47:07 +0300

    Homework 1

---
+++ hw_1/task_1.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+URL = u'https://api.github.com/users/'
+USERNAME = 'j2cry'
+
+
+response = requests.get(f'{URL}{USERNAME}/repos')
+
+print(f'Requesting {URL}{USERNAME}/repos')
+if response.status_code != 200:
+    exit(f'It seems that something went wrong... (error {response.status_code})')
+repositories = response.json()
+
+print(f'User `{USERNAME}` has the following public repositories on GitHub:')
+for repo in repositories:
+    print(f' * {repo.get("name")}')
+
+with open(f'repos_{USERNAME}.json', 'w') as f:
+    json.dump(repositories, f)
+print('done. Check out the result.')
