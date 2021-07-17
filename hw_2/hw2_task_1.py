8c53a1f455dcaf829ef7456721e5244ee4fe5cba
avagadro <j2cry.avagadro@gmail.com>
2021-07-18 02:58:38 +0300

    Homework 2: add SJ parsing

--- hw_2/hw2_task_1.py
+++ hw_2/hw2_task_1.py
@@ -6,35 +6,14 @@ from bs4 import BeautifulSoup as bs
 
 
 URL_HH = 'https://hh.ru/search/vacancy'
-URL_SJ = 'https://www.superjob.ru/'
+URL_SJ = 'https://www.superjob.ru/vacancy/search/'
 df_cols = ['Title', 'Salary (min)', 'Salary (max)', 'Currency', 'Link', 'Source']
-vacancy = 'Data scientist'
-pages = 3
-
-# vacancies = {'title': str,
-#              'min_salary': float,
-#              'max_salary': float,
-#              'link': str,
-#              'source': str}
+vacancy = 'Data scientist'      # job title
+pages = 3                       # amount of pages to parse on each site
 
 
-def parse_hh_page(pg_num: int, **kwargs):
-    hdr = kwargs.get('header', Headers(headers=True).generate())
-    vcn = kwargs.get('vacancy', '')
-    vacancy_param = {'text': vcn, 'page': pg_num}
-
-    # request to HeadHunter
-    response = requests.get(URL_HH, params=vacancy_param, headers=hdr)
-    # parsing
-    soup = bs(response.text, 'lxml')
-
-    vacancy_headers = [elem for elem in soup.find_all(class_='vacancy-serp-item__row_header')]
-    titles = [elem.find(class_='resume-search-item__name').text for elem in vacancy_headers]
-    links = [url.get('href') for elem in vacancy_headers for url in elem('a')]
-    salaries = [salary.text.replace('\u202f', '') if (salary := elem.find(class_='vacancy-serp-item__sidebar')) else ''
-                for elem in vacancy_headers]
-    currencies = [spl[-1] if (spl := cur.split()) else '' for cur in salaries]
-
+def parse_salary(salaries):
+    """ Parse salaries to min/max"""
     # parse salaries: min/max
     min_salaries, max_salaries = [], []
     for sal in salaries:
@@ -52,28 +31,66 @@ def parse_hh_page(pg_num: int, **kwargs):
         else:
             min_salaries.append('')
             max_salaries.append('')
-
-    return pd.DataFrame([titles, min_salaries, max_salaries, currencies, links, ['https://hh.ru/'] * len(titles)],
-                        index=df_cols).T
+    return min_salaries, max_salaries
 
 
-def parse_sj_page(pg_num: int, **kwargs):
+def parse_hh_page(pg_num: int, **kwargs):
+    """ Parse one page on HeadHunter site """
+    home_url = 'https://hh.ru'
     hdr = kwargs.get('header', Headers(headers=True).generate())
     vcn = kwargs.get('vacancy', '')
     vacancy_param = {'text': vcn, 'page': pg_num}
 
-    # request to SuperJob
-    # response = requests.get(URL_SJ, params=vacancy_param, headers=hdr)
+    # request to HeadHunter
+    response = requests.get(URL_HH, params=vacancy_param, headers=hdr)
     # parsing
-    # soup = bs(response.text, 'lxml')
+    soup = bs(response.text, 'lxml')
+
+    vacancy_headers = [elem for elem in soup.find_all(class_='vacancy-serp-item__row_header')]
+    titles = [elem.find(class_='resume-search-item__name').text for elem in vacancy_headers]
+    links = [url.get('href') for elem in vacancy_headers for url in elem('a')]
+    salaries = [salary.text.replace('\u202f', '') if (salary := elem.find(class_='vacancy-serp-item__sidebar')) else ''
+                for elem in vacancy_headers]
+    currencies = [spl[-1] if (spl := cur.split()) else '' for cur in salaries]
+
+    # parse salaries: min/max
+    min_salaries, max_salaries = parse_salary(salaries)
+
+    return pd.DataFrame([titles, min_salaries, max_salaries, currencies, links, [home_url] * len(titles)],
+                        index=df_cols).T
+
+
+def parse_sj_page(pg_num: int, **kwargs):
+    """ Parse one page on SuperJob site """
+    home_url = 'https://www.superjob.ru'
+    hdr = kwargs.get('header', Headers(headers=True).generate())
+    vcn = kwargs.get('vacancy', '')
+    vacancy_param = {'keywords': vcn, 'page': pg_num}
+
+    # request to SuperJob
+    response = requests.get(URL_SJ, params=vacancy_param, headers=hdr)
+    # parsing
+    soup = bs(response.text, 'lxml')
+    titles = [elem.text for elem in soup.find_all(class_='_6AfZ9')]
+    links = [f'{home_url}{elem.get("href")}' for elem in soup.find_all(class_='_6AfZ9')]
+    salaries = [elem.text.replace('\xa0', '') for elem in soup.find_all(class_=['_1qw9T'])]
+    currencies = [regex.group(1) if (regex := re.search(r'\d(\D+)/', cur)) else '' for cur in salaries]
+
+    # parse salaries: min/max
+    min_salaries, max_salaries = parse_salary(salaries)
+
+    return pd.DataFrame([titles, min_salaries, max_salaries, currencies, links, [home_url] * len(titles)],
+                        index=df_cols).T
 
 
 if __name__ == '__main__':
     header = Headers(headers=True).generate()
+
     df = pd.DataFrame(columns=df_cols)
     for pg in range(pages):
+        df = df.append(parse_sj_page(pg, header=header, vacancy=vacancy), ignore_index=True)
         df = df.append(parse_hh_page(pg, header=header, vacancy=vacancy), ignore_index=True)
 
+    df.drop_duplicates(inplace=True, ignore_index=True)
     df.index = df.index + 1
     df.to_csv('vacancies.csv')
-
