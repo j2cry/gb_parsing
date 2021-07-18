ba31eeca83b4bcaca9588612d49f80dcc0733dab
avagadro <j2cry.avagadro@gmail.com>
2021-07-18 21:20:22 +0300

    Homework 2: insert if not exists in DB Homework 3: insert_if_not_exists implemented

--- hw_3/hw3_task_2.py
+++ hw_3/hw3_task_2.py
@@ -1,38 +1,44 @@
 import pandas as pd
 from pymongo import MongoClient
 
-MONGO_HOST = 'mongodb://localhost:27017/'
-DB_NAME = 'gb_homework'
-COLLECTION_NAME = 'vacancies'
 
 CROSS = {'USD': 74.2197}
-BASE_CURRENCY = 'руб.'
-minimal_salary = 90000
 
 
 def recalculate_salary(row):
     return row.min_salary if row.currency not in CROSS.keys() else float(row.min_salary) * CROSS[row.currency]
 
 
-# get database and collection
-client = MongoClient(MONGO_HOST)
-db = client[DB_NAME]
-db_collection = db[COLLECTION_NAME]
+def filter_vacancies(**kwargs):
+    db_host = kwargs.get('host', 'mongodb://localhost:27017/')
+    db_name = kwargs.get('database', 'gb_homework')
+    collection_name = kwargs.get('collection', 'vacancies')
+    base_currency = kwargs.get('currency', 'руб.')
+    minimal_salary = kwargs.get('minimal_salary', 90000)
 
-# request to MongoDB
-query = db_collection.find(filter={'min_salary': {'$ne': ''}, 'currency': {'$ne': ''}},
-                           projection={'_id': 1, 'min_salary': 1, 'currency': 1})
-data = list(query)
+    # get database and collection
+    client = MongoClient(db_host)
+    db = client[db_name]
+    collection = db[collection_name]
 
-# cast to DataFrame and recalculate salaries if required
-df = pd.DataFrame(data)
-cond = df['currency'] != BASE_CURRENCY
-df.loc[cond, 'min_salary'] = df.loc[cond, ['min_salary', 'currency']].apply(recalculate_salary, axis=1)
+    # request to MongoDB
+    query = collection.find(filter={'min_salary': {'$ne': ''}, 'currency': {'$ne': ''}},
+                            projection={'_id': 1, 'min_salary': 1, 'currency': 1})
+    data = list(query)
 
-ids = df.loc[df['min_salary'].astype('float') > minimal_salary, '_id']
+    # cast to DataFrame and recalculate salaries if required
+    df = pd.DataFrame(data)
+    cond = df['currency'] != base_currency
+    df.loc[cond, 'min_salary'] = df.loc[cond, ['min_salary', 'currency']].apply(recalculate_salary, axis=1)
 
-# get full info about vacancies from MongoDB
-query = db_collection.find(filter={'_id': {'$in': ids.tolist()}}, projection={'_id': 0})
-data = list(query)
-df = pd.DataFrame(data)
-df.to_csv('vacancies_filtered.csv')
+    ids = df.loc[df['min_salary'].astype('float') > minimal_salary, '_id']
+
+    # get full info about vacancies from MongoDB
+    query = collection.find(filter={'_id': {'$in': ids.tolist()}}, projection={'_id': 0})
+    data = list(query)
+    df = pd.DataFrame(data)
+    df.to_csv('vacancies_filtered.csv')
+
+
+if __name__ == '__main__':
+    filter_vacancies(minimal_salary=80000)
---
+++ hw_3/hw3_task_3.py
@@ -0,0 +1,29 @@
+from typing import List
+from pymongo import MongoClient
+
+
+def insert_if_not_exists(elements: List[dict], **kwargs):           # testing in hw2_task_1.py
+    db_host = kwargs.get('host', 'mongodb://localhost:27017/')
+    db_name = kwargs.get('database', 'gb_homework')
+    collection_name = kwargs.get('collection', 'vacancies')
+
+    # get database and collection
+    client = MongoClient(db_host)
+    db = client[db_name]
+    collection = db[collection_name]
+
+    for elem in elements[:]:
+        if collection.count_documents(elem) > 0:
+            elements.remove(elem)
+    if elements:
+        collection.insert_many(elements)
+
+
+if __name__ == '__main__':
+    pass
+    # check DB content
+    # cl = MongoClient('mongodb://localhost:27017/')
+    # coll = cl.gb_homework.vacancies
+    #
+    # for num, el in enumerate(coll.find()):
+    #     print(num + 1, el)
