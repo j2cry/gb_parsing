30741a923e2799fb1c30df5a0ecc64f23593dbd8
avagadro <j2cry.avagadro@gmail.com>
2021-07-24 02:42:13 +0300

    Homework 5: add readme.txt

--- hw_5/job_scraper/job_run.py
+++ hw_5/job_scraper/job_run.py
@@ -1,7 +1,7 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 from hw_5.job_scraper import settings
-from hw_5.job_scraper.spiders.job_scraper import JobScraperSpider
+from hw_5.job_scraper.spiders.job import JobScraperSpider
 
 
 if __name__ == '__main__':
