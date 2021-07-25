efe8005f4de8733ce5bebc72537ac4d9d3d42e94
avagadro <j2cry.avagadro@gmail.com>
2021-07-25 22:43:26 +0300

    Homework 5: implemented Job ItemLoader

--- hw_5/job_scraper/spiders/job.py
+++ hw_5/job_scraper/spiders/job.py
@@ -1,5 +1,6 @@
 import re
 import scrapy
+from itemloaders import ItemLoader, processors
 from scrapy.http import HtmlResponse
 from urllib.parse import urlparse
 from hw_5.job_scraper.items import JobScraperItem
@@ -28,12 +29,13 @@ class JobScraperSpider(scrapy.Spider):
     allowed_domains = ['hh.ru', 'superjob.ru']
     # allowed_domains = ['hh.ru']           # если надо разделить на 2 паука
     # allowed_domains = ['superjob.ru']
-    start_urls = [f'https://hh.ru/search/vacancy?text={job_title}', f'https://www.superjob.ru/vacancy/search/?keywords={job_title}']
+    start_urls = [f'https://hh.ru/search/vacancy?text={job_title}',
+                  f'https://www.superjob.ru/vacancy/search/?keywords={job_title}']
     # start_urls = [f'https://hh.ru/search/vacancy?text={job_title}']       # если надо разделить на 2 паука
     # start_urls = [f'https://www.superjob.ru/vacancy/search/?keywords={job_title}']
     rules = None
 
-    def parse(self, response: HtmlResponse):
+    def parse(self, response: HtmlResponse, **kwargs):
         # get parsing rules for current domain
         for domain in self.allowed_domains:
             if response.url.find(domain) > 0:
@@ -51,14 +53,16 @@ class JobScraperSpider(scrapy.Spider):
             yield response.follow(link, callback=self.vacancy_parse)
 
     def vacancy_parse(self, response: HtmlResponse):
-        # get title, salaries+currency, link, source
-        title = response.xpath(self.rules.get('title')).get('')
-        salary = ' '.join(response.xpath(self.rules.get('salary')).getall())
+        vacancy = ItemLoader(item=JobScraperItem(), selector=response)
+        vacancy.default_output_processor = processors.Join(' ')
+        vacancy.add_xpath('title', self.rules.get('title'))
         link = response.url[:index] if (index := response.url.find('?')) != -1 else response.url
+        vacancy.add_value('link', link)
         parsed_link = urlparse(link)
-        source = f'{parsed_link.scheme}://{parsed_link.netloc}/'
+        vacancy.add_value('source', f'{parsed_link.scheme}://{parsed_link.netloc}/')
 
         # parse salary and currency
+        salary = ' '.join(response.xpath(self.rules.get('salary')).getall())
         salary = salary.replace('\xa0', '') if salary else ''
         regex = re.findall(r'(\d+)', salary)
         salary_switcher = {0: lambda: ('', ''),
@@ -67,9 +71,8 @@ class JobScraperSpider(scrapy.Spider):
         min_salary, max_salary = salary_switcher[len(regex)]()
         currency = spl[-1] if (spl := salary.split()) and (len(regex) > 0) else ''
 
-        yield JobScraperItem(title=title,
-                             min_salary=min_salary,
-                             max_salary=max_salary,
-                             currency=currency,
-                             link=link,
-                             source=source)
+        vacancy.add_value('min_salary', min_salary)
+        vacancy.add_value('max_salary', max_salary)
+        vacancy.add_value('currency', currency)
+
+        yield vacancy.load_item()
