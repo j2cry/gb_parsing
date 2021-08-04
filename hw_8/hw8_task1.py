18846d44c8643d064657ae06ff784d7d48135630
avagadro <j2cry.avagadro@gmail.com>
2021-08-04 16:36:29 +0300

    Homework 8: impl. unzipping & preparing dataset

--- hw_8/hw8_task1.py
+++ hw_8/hw8_task1.py
@@ -1,4 +1,8 @@
+import os
 import pathlib
+import urllib.request
+import zipfile
+
 import pandas as pd
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
@@ -21,37 +25,54 @@ class MosOpenDataPreparer:
         self.file_types = file_types
 
     def get(self, ds_num: int):
-        # (//li[@itemscope="itemscope"])[...]/a/div/span[@class="dataset-number"]
-        # (//span[@id="dropDepartmentsLink"])[140]
         ds_xpath = f'(//span[@id="dropDepartmentsLink"])[{ds_num if ds_num > 0 else 1}]'
 
         self.driver.get('https://data.mos.ru/opendata')
         try:
             data_element = self.driver.find_element_by_xpath(ds_xpath)
         except NoSuchElementException:
-            return False
+            return
         ActionChains(self.driver).move_to_element(data_element).perform()
         data_element.click()
-        data_link = None
+        data_link, file_type = None, None
         # check available types
-        for file_type in self.file_types:
+        for ft in self.file_types:
             try:
-                data_link = self.driver.find_element_by_xpath(f'//ul[@id="dropExport"]//a[text()="{file_type}"]')
+                data_link = self.driver.find_element_by_xpath(f'//ul[@id="dropExport"]//a[text()="{ft}"]')
+                file_type = ft
                 break
             except NoSuchElementException:
                 pass
         if not data_link:       # no available types found
-            return False
+            return
         data_link = data_link.get_attribute('href')
+        self.driver.close()
 
-        self.driver.get(data_link)
+        cwd = pathlib.Path().cwd()
+        tf_path = cwd.joinpath('tempfile')
+        urllib.request.urlretrieve(data_link, tf_path)
+        # unzip downloaded file
+        with zipfile.ZipFile(tf_path, 'r') as zip_file:
+            zip_file.extractall(cwd)
+        tf_path.unlink()
 
-        # self.driver.close()
+        for filename in os.listdir(cwd):
+            if filename.endswith(file_type):
+                self.prepare(pathlib.Path(filename), file_type)
 
-    def __prepare(self, df: pd.DataFrame):
-        pass
+    @staticmethod
+    def prepare(file_path, file_type):
+        ft_switcher = {'csv': lambda: pd.read_csv(file_path),
+                       'xlsx': lambda: pd.read_excel(file_path)}
+        if file_type not in ft_switcher.keys():
+            return False
+        df = ft_switcher[file_type]()
+        df.dropna(axis=1, inplace=True)     # drop NaN columns
+        df_path = pathlib.Path(file_path).name.replace(file_type, 'csv')
+        df.to_csv(df_path)
 
 
 if __name__ == '__main__':
     pre = MosOpenDataPreparer()
     pre.get(140)
+    # pre.prepare()
